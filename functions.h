6936af0a312a12a175885d8dda116772e801d17d
Alexandro <alexx1098@outlook.es>
2021-03-18 01:11:44 -0600

    Fixed some bugs and improved the code

--- functions.h
+++ functions.h
@@ -2,60 +2,100 @@
 #include <math.h>
 
 // ? Funciones de calculadora básicas
-//* suma
-float Add(float num1, float num2)
+//* Suma
+int Add()
 {
-  float sum;
+  int n, sum = 0, c, nums[100];
 
-  sum = num1 + num2;
+  printf("Ingresa la cantidad de numeros a sumar\n");
+  scanf("%d", &n);
+
+  for (c = 0; c < n; c++)
+  {
+    printf("Ingresa el numero %i: ", c);
+    scanf("%d", &nums[c]);
+    sum += nums[c];
+  }
+
+  printf("El resultado de la suma es = %d\n", sum);
 
   return sum;
 }
-//* resta
+//* Resta
 float Subtraction(float num1, float num2)
 {
-  float substract;
+  float subt = 0;
 
-  substract = num1 - num2;
+  printf("Ingresa el valor a restar numero 1: ");
+  scanf("%f", &num1);
+  printf("Ingresa el valor a restar numero 2: ");
+  scanf("%f", &num2);
 
-  return substract;
+  subt = num1 - num2;
+
+  printf("El resultado de la resta es = %.2f\n", subt);
+
+  return subt;
 }
 //* multiplicacion
 float Multiply(float num1, float num2)
 {
-  float multiplication;
+  float mult = 0;
 
-  multiplication = num1 * num2;
+  printf("Ingresa el valor a multiplicar numero 1: ");
+  scanf("%f", &num1);
+  printf("Ingresa el valor a multiplicar numero 2: ");
+  scanf("%f", &num2);
 
-  return multiplication;
+  mult = num1 * num2;
+
+  printf("El resultado de la multiplicacion es = %.2f\n", mult);
+
+  return mult;
 }
 //* division
 float Divide(float num1, float num2)
 {
-  float division;
+  float division = 0;
+
+  printf("Ingresa el valor a dividir numero 1: ");
+  scanf("%f", &num1);
+  printf("Ingresa el valor a dividir numero 2: ");
+  scanf("%f", &num2);
 
   division = num1 / num2;
 
+  printf("El resultado de la division es = %.2f\n", division);
+
   return division;
 }
-
-// ? Tipos de elevacion
 
 //* raiz cuadrada
 float Square_Root(float num1)
 {
   float squareRoot;
 
+  printf("Ingresa un numero a sacar raiz cuadrada\n");
+  scanf("%f", &num1);
+
   squareRoot = sqrt(num1);
+
+  printf("La raiz cuadrada del numoer es = %.2f\n", squareRoot);
 
   return squareRoot;
 }
+// ? Tipos de elevacion
 //* elevacion al cuadrado
 float Square_Elevation(float num1)
 {
   float squareElev;
 
+  printf("Ingresa un numero a elevar al cuadrado.\n");
+  scanf("%f", &num1);
+
   squareElev = pow(num1, 2);
+
+  printf("El cuadrado del numero es = %.2f\n", squareElev);
 
   return squareElev;
 }
@@ -64,7 +104,12 @@ float Cube_Elevation(float num1)
 {
   float cubeElev;
 
+  printf("Ingresa un numero a elevar al cubo.\n");
+  scanf("%f", &num1);
+
   cubeElev = pow(num1, 3);
+
+  printf("El cubo del numero, es: %.2f\n", cubeElev);
 
   return cubeElev;
 }
@@ -73,7 +118,12 @@ float Fourth_Elevation(float num1)
 {
   float fourthElev;
 
+  printf("Ingresa un numero a elevar a la cuarta.\n");
+  scanf("%f", &num1);
+
   fourthElev = pow(num1, 4);
+
+  printf("La elevacion a la cuarta del numero, es: %.2f\n", fourthElev);
 
   return fourthElev;
 }
@@ -86,8 +136,13 @@ float Sine(float num1)
 {
   float sine, degree;
 
+  printf("Ingresa un numero a obtener su seno\n");
+  scanf("%f", &num1);
+
   degree = num1 * (PI / 180);
   sine = sin(degree);
+
+  printf("El seno del numero, es: %.2f\n", sine);
 
   return sine;
 }
@@ -96,8 +151,13 @@ float Cosen(float num1)
 {
   float cosine, degree;
 
+  printf("Ingresa un numero a obtener su coseno\n");
+  scanf("%f", &num1);
+
   degree = num1 * (PI / 180);
   cosine = cos(degree);
+
+  printf("El coseno del numero, es: %.2f\n", cosine);
 
   return cosine;
 }
@@ -106,8 +166,13 @@ float Tangent(float num1)
 {
   float tangent, degree;
 
+  printf("Ingresa un numero a obtener su tangente\n");
+  scanf("%f", &num1);
+
   degree = num1 * (PI / 180);
   tangent = tan(degree);
+
+  printf("La tangente del numero, es: %.2f\n", tangent);
 
   return tangent;
 }
@@ -116,8 +181,13 @@ float Cosecant(float num1)
 {
   float coSecant, degree;
 
+  printf("Ingresa un numero a obtener su cosecante\n");
+  scanf("%f", &num1);
+
   degree = num1 * (PI / 180);
   coSecant = 1 / sin(degree);
+
+  printf("La cosecante del numero, es: %.2f\n", coSecant);
 
   return coSecant;
 }
@@ -126,8 +196,13 @@ float Secant(float num1)
 {
   float secant, degree;
 
+  printf("Ingresa un numero a obtener su secante\n");
+  scanf("%f", &num1);
+
   degree = num1 * (PI / 180);
   secant = 1 / cos(degree);
+
+  printf("La secante del numero, es: %.2f\n", secant);
 
   return secant;
 }
@@ -136,8 +211,13 @@ float Cotangent(float num1)
 {
   float cotangent, degree;
 
+  printf("Ingresa un numero a obtener su cotangente\n");
+  scanf("%f", &num1);
+
   degree = num1 * (PI / 180);
   cotangent = 1 / tan(degree);
+
+  printf("La cotangente del numero, es: %.2f\n", cotangent);
 
   return cotangent;
 }
@@ -147,7 +227,12 @@ float NaturalLogarithm(float num1)
 {
   float Log;
 
+  printf("Ingresa un numero a sacar su logaritmo natural\n");
+  scanf("%f", &num1);
+
   Log = log(num1);
+
+  printf("El logaritmo del numero, es: %.2f\n", Log);
 
   return Log;
 }
@@ -155,7 +240,12 @@ float Base10Logarithm(float num1)
 {
   float loga10;
 
+  printf("Ingresa un numero a sacar su logaritmo base 10\n");
+  scanf("%f", &num1);
+
   loga10 = log10(num1);
+
+  printf("El logaritmo base 10 del numero, es: %.2f\n", loga10);
 
   return loga10;
 }
@@ -184,12 +274,13 @@ int Factorial()
 float Porcentage(float num1, float num2)
 {
   float percentage;
-  printf("Ingresa la cantidad a sacar porcentaje y el porcentaje a sacar: \n");
+
+  printf("Ingresa el numero y la cantidad de porcentaje a sacar: \n");
   scanf("%f%f", &num1, &num2);
 
   percentage = (num1 * num2) / 100;
 
-  printf("El porcentaje de %.2f, es: \n%.2f", num1, num2);
+  printf("El porcentaje de %.2f es = %.2f\n", num1, percentage);
 
   return percentage;
 }
@@ -202,7 +293,6 @@ int i, j, rws, clms;
 //? Matrix addition.
 int MatrixAddition()
 {
-
   //! Getting values.
 
   printf("Introduce las filas de las matrices: \n");
@@ -247,7 +337,7 @@ int MatrixAddition()
   {
     for (j = 0; j < clms; j++)
     {
-      printf("%d", sum[i][j]);
+      printf("[%d] ", sum[i][j]);
       if (j == clms - 1)
       {
         printf("\n");
@@ -304,7 +394,7 @@ int MatrixSubstraction()
   {
     for (j = 0; j < clms; j++)
     {
-      printf("%d", subst[i][j]);
+      printf("[%d] ", subst[i][j]);
       if (j == clms - 1)
       {
         printf("\n");
@@ -361,7 +451,7 @@ int MatrixMultip()
   {
     for (j = 0; j < clms; j++)
     {
-      printf("%d", mult[i][j]);
+      printf("[%d] ", mult[i][j]);
       if (j == clms - 1)
       {
         printf("\n");
--- mainProgram.c
+++ mainProgram.c
@@ -12,6 +12,7 @@ int main()
 
   for (start = 0; start >= 0; start++)
   {
+
     printf("\n\t\t Calculadora Cientifica.\n");
 
     printf("Ingresa la operacion deseada a realizar.\n");
@@ -20,7 +21,8 @@ int main()
     printf("5.- Raiz cuadrada\t\t 6.- Elevacion de un numero\n");
     printf("7.- Funcion trigonometrica\t 8.- Logaritmo de un numero\n");
     printf("9.- Matrices.\t\t\t 10.- Porcentaje.\n");
-    printf("11.- Factorial\t\t\t 0.- Salir\n");
+    printf("11.- Factorial.\t\t\t 0.- Salir\n");
+
     scanf("%i", &op);
 
     // ? Operaciones basicas
@@ -30,37 +32,27 @@ int main()
       return 0;
       break;
     case 1:
-      printf("Ingresa dos numeros a sumar.\n");
-      scanf("%f%f", &numberOne, &numberTwo);
-      printf("El resultado de la suma, es: %.2f\n", Add(numberOne, numberTwo));
+      printf("", Add());
       system("\npause");
       system("cls");
       break;
     case 2:
-      printf("Ingresa dos numeros a restar.\n");
-      scanf("%f%f", &numberOne, &numberTwo);
-      printf("El resultado de la resta, es: %.2f\n", Subtraction(numberOne, numberTwo));
+      printf("", Subtraction(numberOne, numberTwo));
       system("\npause");
       system("cls");
       break;
     case 3:
-      printf("Ingresa dos numeros a multiplicar.\n");
-      scanf("%f%f", &numberOne, &numberTwo);
-      printf("El resultado de la multiplicacion, es: %.2f\n", Multiply(numberOne, numberTwo));
+      printf("", Multiply(numberOne, numberTwo));
       system("\npause");
       system("cls");
       break;
     case 4:
-      printf("Ingresa dos numeros a dividir.\n");
-      scanf("%f%f", &numberOne, &numberTwo);
-      printf("El resultado de la division, es: %.2f\n", Divide(numberOne, numberTwo));
+      printf("", Divide(numberOne, numberTwo));
       system("\npause");
       system("cls");
       break;
     case 5:
-      printf("Ingresa el numero a sacar raiz.\n");
-      scanf("%f", &numberOne);
-      printf("La raiz cuadrada del numero, es: %.2f\n", Square_Root(numberOne));
+      printf("", Square_Root(numberOne));
       system("\npause");
       system("cls");
       break;
@@ -76,23 +68,17 @@ int main()
       switch (tipe)
       {
       case 1:
-        printf("Ingresa un numero a elevar al cuadrado.\n");
-        scanf("%f", &numberOne);
-        printf("El cuadrado del numero, es: %.2f\n", Square_Elevation(numberOne));
+        printf("", Square_Elevation(numberOne));
         system("\npause");
         system("cls");
         break;
       case 2:
-        printf("Ingresa un numero a elevar al cubo.\n");
-        scanf("%f", &numberOne);
-        printf("El cubo del numero, es: %.2f\n", Cube_Elevation(numberOne));
+        printf("", Cube_Elevation(numberOne));
         system("\npause");
         system("cls");
         break;
       case 3:
-        printf("Ingresa un numero a elevar a la cuarta.\n");
-        scanf("%f", &numberOne);
-        printf("La elevacion a la cuarta del numero, es: %.2f\n", Fourth_Elevation(numberOne));
+        printf("", Fourth_Elevation(numberOne));
         system("\npause");
         system("cls");
         break;
@@ -112,44 +98,32 @@ int main()
       switch (tipe)
       {
       case 1:
-        printf("Ingresa un numero a obtener su seno\n");
-        scanf("%f", &numberOne);
-        printf("El seno del numero, es: %.2f\n", Sine(numberOne));
+        printf("", Sine(numberOne));
         system("\npause");
         system("cls");
         break;
       case 2:
-        printf("Ingresa un numero a obtener su coseno\n");
-        scanf("%f", &numberOne);
-        printf("El coseno del numero, es: %.2f\n", Cosen(numberOne));
+        printf("", Cosen(numberOne));
         system("\npause");
         system("cls");
         break;
       case 3:
-        printf("Ingresa un numero a obtener su tangente\n");
-        scanf("%f", &numberOne);
-        printf("La tangente del numero, es: %.2f\n", Tangent(numberOne));
+        printf("", Tangent(numberOne));
         system("\npause");
         system("cls");
         break;
       case 4:
-        printf("Ingresa un numero a obtener su cosecante\n");
-        scanf("%f", &numberOne);
-        printf("La cosecante del numero, es: %.2f\n", Cosecant(numberOne));
+        printf("", Cosecant(numberOne));
         system("\npause");
         system("cls");
         break;
       case 5:
-        printf("Ingresa un numero a obtener su secante\n");
-        scanf("%f", &numberOne);
-        printf("La secante del numero, es: %.2f\n", Secant(numberOne));
+        printf("", Secant(numberOne));
         system("\npause");
         system("cls");
         break;
       case 6:
-        printf("Ingresa un numero a obtener su cotangente\n");
-        scanf("%f", &numberOne);
-        printf("La cotangente del numero, es: %.2f\n", Cotangent(numberOne));
+        printf("", Cotangent(numberOne));
         system("\npause");
         system("cls");
         break;
@@ -167,16 +141,12 @@ int main()
       switch (tipe)
       {
       case 1:
-        printf("Ingresa un numero a sacar su logaritmo natural\n");
-        scanf("%f", &numberOne);
-        printf("El logaritmo del numero, es: %.2f\n", NaturalLogarithm(numberOne));
+        printf("", NaturalLogarithm(numberOne));
         system("\npause");
         system("cls");
         break;
       case 2:
-        printf("\tIngresa un numero a sacar su logaritmo base 10\n");
-        scanf("%f", &numberOne);
-        printf("El logaritmo base 10 del numero, es: %.2f\n", Base10Logarithm(numberOne));
+        printf("", Base10Logarithm(numberOne));
         system("\npause");
         system("cls");
         break;
@@ -198,17 +168,17 @@ int main()
       switch (tipe)
       {
       case 1:
-        printf(" ", MatrixAddition());
+        printf("", MatrixAddition());
         system("\npause");
         system("cls");
         break;
       case 2:
-        printf(" ", MatrixSubstraction());
+        printf("", MatrixSubstraction());
         system("\npause");
         system("cls");
         break;
       case 3:
-        printf(" ", MatrixMultip());
+        printf("", MatrixMultip());
         system("\npause");
         system("cls");
         break;
